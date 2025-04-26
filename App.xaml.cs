3b979a965bf6ce677d7fa734586b8d1dae64ce1d
Ventura <ventura.py3@gmail.com>
2025-04-25 21:41:02 -0300

    fix: Novo projeto

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LumeClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
